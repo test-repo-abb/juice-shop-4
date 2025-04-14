d2dcbfcf76f3b9e35fd99c84ab5e67b6d69df254
Dmytro Lynda <dmytrolynda@gmail.com>
2025-04-14 15:24:21 +0200

    Add code with violation

--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -18,5 +18,16 @@ module.exports = function searchProducts () {
       }).catch((error: ErrorWithParent) => {
         next(error.parent)
       })
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
+      .then(([products]: any) => {
+        const dataString = JSON.stringify(products)
+        for (let i = 0; i < products.length; i++) {
+          products[i].name = req.__(products[i].name)
+          products[i].description = req.__(products[i].description)
+        }
+        res.json(utils.queryResultToJson(products))
+      }).catch((error: ErrorWithParent) => {
+        next(error.parent)
+      })
   }
 }
